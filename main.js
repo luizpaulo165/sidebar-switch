974d0452c25b52ddb2f8ab8cd1965b16f6da907b
Luiz Paulo Valença <luizpaulo165@gmail.com>
2017-09-04 10:39:38 -0300

    Adjs

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-define(['require', 'exports', 'module'], function (require, exports, module) {
-	
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets */
+
+/** Simple extension that adds a "File > Hello World" menu item. Inserts "Hello, world!" at cursor pos. */
+define(function (require, exports, module) {
+    'use strict';
+    
+//   var CommandManager = brackets.getModule("command/CommandManager"),
+//        EditorManager  = brackets.getModule("editor/EditorManager"),
+//        Menus          = brackets.getModule("command/Menus");
+    
+    var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
+    
+    ExtensionUtils.loadStyleSheet(module, 'sidebar-switch.css');
 });
